cdcb0cb226d5e276c40cfc23a75d92656ba04ec0
marimuthut <rockmarimuthu@gmail.com>
2017-04-06 16:53:55 +0530

    .

--- MinitrelloApp/src/js/directives/focusInput.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module('app')
-    .directive('focusInput', function () {
-      return {
-        link: function (scope, element, attrs) {
-          element[0].focus();
-        }
-      };
-    });
